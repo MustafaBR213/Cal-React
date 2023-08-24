63ada84d029509c552c4bce9f82bd0e1c61cc7b7
MustafaBR213 <brokymostafa@gmail.com>
2023-08-25 02:27:16 +0300

    first commit

---
+++ src/DigitButton.js
@@ -0,0 +1,5 @@
+import { ACTIONS } from "./App"
+
+export function DigitButton({dispatch, digit}) {
+    return <button onClick={() => dispatch({type: ACTIONS.ADD_DIGIT, payload:{digit}})}>{digit}</button>
+}
---
+++ src/OperationButton.js
@@ -0,0 +1,8 @@
+import { ACTIONS } from "./App"
+
+export function OperationButton({dispatch, operation}) {
+    return <button onClick={() => dispatch({type: ACTIONS.CHOOSE_OPERATION, payload:{operation}})}
+    >
+        {operation}
+    </button>
+}
